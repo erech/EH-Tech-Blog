3baa7b482f35186d74919bb7ba7a0fa6ef327e78
erech <sofiahanapin@gmail.com>
2023-03-09 03:59:54 -0800

    install packages + created server.js

---
+++ server.js
@@ -0,0 +1,42 @@
+
+//declare constants
+const express = require('express');
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const path = require('path');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+const store = new SequelizeStore({ db: sequelize });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+const sesh = {
+  secret: 'bigbluedog',
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store
+};
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sesh));
+app.use(routes);
+
+async function startServer() {
+    await sequelize.sync({ force: false });
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+  }
+  
+  startServer();
